4f61d747d4366ff4650bdac668ba45ef3c2595a6
shane <sssims@ucsc.edu>
2015-02-10 19:52:22 -0800

    did i destroy everything

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,7 @@
 lock '3.1.0'
 
 set :application, 'shanesims_me'
-set :repo_url, 'git@github.com:sssims/shanesims_me.git'
-
-set :deploy_to, '/home/deploy/shanesims_me'
-
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -45,21 +40,19 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  after :publishing, :restart
 
-
-  #after :restart, :clear_cache do
-  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #    # Here we can do anything such as:
-  #   # within release_path do
-  #    #   execute :rake, 'cache:clear'
-  #    # end
-  # end
-  #end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
 
 end
