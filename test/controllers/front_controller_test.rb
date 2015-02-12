749585442df702fa82a08649019dc08e2f3a1a1a
shane <sssims@ucsc.edu>
2015-02-11 20:53:34 -0800

    version 1.0.0

--- test/controllers/front_controller_test.rb
+++ test/controllers/front_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TestControllerTest < ActionController::TestCase
+class FrontControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
